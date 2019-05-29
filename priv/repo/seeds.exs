10ddeca83eceb900c5d3c53481144f692bc44485
Matias Bastos <matias.bastos@xcaliber.com>
2019-05-29 13:38:46 +0200

    seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,35 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+BooksApi.Repo.insert! %BooksApi.Accounts.User{
+  id: 1,
+  username: "admin",
+  password: BooksApi.Accounts.hash_password("admin")
+}
+BooksApi.Repo.insert! %BooksApi.Accounts.Method{
+  user_id: 1,
+  method: "create"
+}
+BooksApi.Repo.insert! %BooksApi.Accounts.Method{
+  user_id: 1,
+  method: "update"
+}
+BooksApi.Repo.insert! %BooksApi.Accounts.Method{
+  user_id: 1,
+  method: "delete"
+}
+BooksApi.Repo.insert! %BooksApi.Accounts.Method{
+  user_id: 1,
+  method: "show"
+}
+BooksApi.Repo.insert! %BooksApi.Books.Author{
+  id: 1,
+  firstname: "some",
+  lastname: "dude"
+}
+BooksApi.Repo.insert! %BooksApi.Books.Book{
+  id: 1,
+  name: "a book",
+  description: "some description",
+  author_id: 1
+}
